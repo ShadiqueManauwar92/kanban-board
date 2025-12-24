8844bacb3f27d9514089c54d41e7909d378ac597
ShadiqueManauwar <manauwarshadique07@gmail.com>
2025-12-25 01:07:19 +0530

    corrected the route

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { serverRoutes } from './app.routes.server';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(serverRoutes),
+    provideZonelessChangeDetection(),
     provideAnimations()
   ]
 };
